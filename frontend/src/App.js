79d53f5d30f57bc3471a169fe4ebb2d97cd3e6c4
Vivekdhameliya2 <vivekdhameliya4@gmail.com>
2024-04-30 10:56:10 -0400

    added loader while search

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
-import "./styles.css" // Importing the CSS file
+import "./styles.css"; // Importing the CSS file
 import SearchBar from './components/SearchBar';
 import ResultsList from './components/ResultsList';
 import DetailedView from './components/DetailedView';
@@ -10,16 +10,14 @@ function App() {
     const [results, setResults] = useState([]);  // Holds search results (list of IDs)
     const [selectedDetail, setSelectedDetail] = useState(null);  // Holds detailed data for a selected ID
     const [error, setError] = useState('');  // Error handling
+    const [loading, setLoading] = useState(false); // Loading indicator state
     const [currentPage, setCurrentPage] = useState(1); // Current page
     const [totalPages, setTotalPages] = useState(0); // Total number of pages
     const itemsPerPage = 4; // Number of items per page
 
     const handleSearch = async (query) => {
         setError('');
-        if (!query) {
-            setError('Please enter a search query.');
-            return;
-        }
+        setLoading(true); // Set loading to true before making the request
 
         try {
             const response = await axios.post('http://localhost:5000/api/search', JSON.stringify({ query }), {
@@ -39,6 +37,8 @@ function App() {
             setError('Failed to fetch data. Please check the server.');
             setResults([]);
             console.error('Search error:', error);
+        } finally {
+            setLoading(false); // Set loading to false after the request completes
         }
     };
 
@@ -63,6 +63,7 @@ function App() {
         <div className="App">
             <h1 className="header">PubMed Search Application</h1>
             <SearchBar onSearch={handleSearch} />
+            {loading && <div className="loader"></div>} {/* Display loader animation */}
             {error && <p style={{ color: 'red' }}>{error}</p>}
             <ResultsList results={results.slice(startIndex, endIndex)} onSelectDetail={fetchDetails} />
             <Pagination currentPage={currentPage} totalPages={totalPages} onPageChange={handlePageChange} className="pagination" />
