b99557c9d9166b03c5cc6fcb5f56d53432718b1d
Vivekdhameliya2 <vivekdhameliya4@gmail.com>
2024-04-30 11:09:51 -0400

    added style in detailedView.js

--- frontend/src/components/DetailedView.js
+++ frontend/src/components/DetailedView.js
@@ -6,14 +6,28 @@ function DetailedView({ details }) {
 
     return (
         <div className="detailed-view">
-            <h3>Publication Details</h3>
-            <p><strong>Title:</strong> {details.Title !== "N/A" ? details.Title : "Title not available"}</p>
-            <p><strong>Abstract:</strong> {details.Abstract !== "N/A" ? details.Abstract : "Abstract not available"}</p>
-            <p><strong>Authors:</strong> {details.AuthorList !== "N/A" ? details.AuthorList : "Author list not available"}</p>
-            <p><strong>Journal:</strong> {details.Journal !== "N/A" ? details.Journal : "Journal information not available"}</p>
-            <p><strong>Year:</strong> {details.PublicationYear !== "N/A" ? details.PublicationYear : "Publication year not available"}</p>
-            <p><strong>MeSH Terms:</strong> {details.MeSHTerms !== "N/A" ? details.MeSHTerms : "MeSH terms not available"}</p>
-            <p><strong>PMID:</strong> {details.PMID !== "N/A" ? details.PMID : "PMID not available"}</p>
+            <h3 className="detail-heading">Publication Details</h3>
+            <div className="detail-item">
+                <strong>Title:</strong> {details.Title !== "N/A" ? details.Title : "Title not available"}
+            </div>
+            <div className="detail-item">
+                <strong>Abstract:</strong> {details.Abstract !== "N/A" ? details.Abstract : "Abstract not available"}
+            </div>
+            <div className="detail-item">
+                <strong>Authors:</strong> {details.AuthorList !== "N/A" ? details.AuthorList : "Author list not available"}
+            </div>
+            <div className="detail-item">
+                <strong>Journal:</strong> {details.Journal !== "N/A" ? details.Journal : "Journal information not available"}
+            </div>
+            <div className="detail-item">
+                <strong>Year:</strong> {details.PublicationYear !== "N/A" ? details.PublicationYear : "Publication year not available"}
+            </div>
+            <div className="detail-item">
+                <strong>MeSH Terms:</strong> {details.MeSHTerms !== "N/A" ? details.MeSHTerms : "MeSH terms not available"}
+            </div>
+            <div className="detail-item">
+                <strong>PMID:</strong> {details.PMID !== "N/A" ? details.PMID : "PMID not available"}
+            </div>
         </div>
     );
 }
